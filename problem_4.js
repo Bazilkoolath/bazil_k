ec2c798a9e978c5d990be861083e14d373320d3a
Bazilkoolath <bazilkoolath7777@gmail.com>
2025-06-25 12:28:31 +0530

    problem_4

---
+++ problem_4.js
@@ -0,0 +1,22 @@
+function countMultiples(inputList) {
+
+    const result = {};
+    for (let i = 1; i <= 9; i++) {
+        result[i] = 0;
+    }
+
+    for (const num of inputList) {
+        for (let i = 1; i <= 9; i++) {
+            if (num % i === 0) {
+                result[i]++;
+            }
+        }
+    }
+
+    return result;
+}
+
+const input = [1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30];
+const output = countMultiples(input);
+console.log(output);
+
